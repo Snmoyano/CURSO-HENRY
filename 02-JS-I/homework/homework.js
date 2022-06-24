54e85747a5d71639208bbf6f316369410f9af556
Nicolas <sergio.nicolas.moyano@gmail.com>
2022-06-24 13:49:24 -0300

    First Homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -156,8 +156,8 @@ function redondearNumero(num) {
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
-}return math.ceil (num)
+  return math.ceil(num)
+
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
@@ -289,3 +289,5 @@ module.exports = {
   deEuroAdolar,
   esVocal,
 };
+
+
